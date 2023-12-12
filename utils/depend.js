25f3ab61ffd42d137c6fb6ecbd84b431bcd153a0
Isiarf <imrfauntleroy@yahoo.com>
2023-12-12 13:44:36 -0800

    Added files to start the project.

---
+++ utils/depend.js
@@ -0,0 +1,12 @@
+const dependendencies = [
+   // inquirer = require('inquirer'),
+    { prompt } = require('enquirer'),
+    fs = require('fs'),
+    jest = require('jest'),
+    questions = require('./quest.js'),
+    { Circle, Square, Triangle } = require ('../lib/shapes.js'),
+
+];
+
+module.exports = dependendencies;
+
