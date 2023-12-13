9c965e74e9076adaabebfff1713130747f9fcbd4
Isiarf <imrfauntleroy@yahoo.com>
2023-12-12 16:18:52 -0800

    Project completed.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 require('./utils/depend.js');
-  
-  prompt(questions)
-    .then((answers) => {
-      console.log('\nAnswers: ', JSON.stringify(answers, null, '  '));
-    })
-    .catch((error) => {
-      console.error('Error:', error);
+
+// Prompt the user to answer questions for their logo
+prompt(questions)
+  .then((answers) => {
+    // Call the shapes constructor function to push the users answers
+    const shape = new Shapes(
+      answers.text,
+      answers.textColor,
+      answers.shapeColor,
+      answers.shape,
+    );
+    // Call the svg spawner function with the answers from the shapes constructor function
+    const generatedLogo = spawnSVG({
+      text: shape.text,
+      textColor: shape.textColor,
+      shape: shape.shape,
+      shapeColor: shape.color,
     });
+    // Create a svg file to put the logo
+    fs.writeFileSync('logo.svg', generatedLogo);
+
+    // Log that the logo has been generated
+    console.log('Logo generated:\n', generatedLogo);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
     
